e7d951452ffe9c773b747cdf68e1f428bd5ad1c4
milotriss <nhattienla98@gmail.com>
2023-12-19 08:04:09 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,56 @@
-
-
-
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
-import Header from "./components/header/header";
-import ListTodo from "./components/listTodo/listTodo";
-
-
-export interface Todo {
-  id:number
-  content:string
-  status:boolean
-}
 
 function App() {
-  const [update,setUpdate] = useState<boolean>(false)
+  const [cards, setCards] = useState<any>([]);
+  const [input,setInput] = useState<string>('');
 
-  const handleUpdate =()=> {
-    setUpdate(!update)
+  useEffect(() => {
+    if (input.length === 0) {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/search/movie?api_key=97f1bd616b51e07825e04a855aaeed30&query=${search}"
+        )
+        .then((data) => setCards(data.data.results))
+        .catch((error) => {
+          console.log(error);
+        });
+    }else{
+      onSearch()
+    }
+  }, [input]);
+  const onSearch = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=97f1bd616b51e07825e04a855aaeed30&query=${input}`
+      )
+      .then((data) => setCards(data.data.results))
+      .catch((error) => {
+        console.log(error);
+      });
   }
-  
   return (
     <div className="App">
-      <Header handleUpdate={handleUpdate}/>
-      <ListTodo update={update} handleUpdate={handleUpdate}/>
+      <header>
+        <input
+        onChange={(e) => setInput(e.target.value)} 
+        autoFocus 
+        placeholder="Search by name ..." 
+        type="text" />
+        <button onClick={onSearch}>Search</button>
+      </header>
+      <main>
+        {cards.map((card: any) => {
+          return (
+            <figure key={card.id}>
+              <img src={card.poster_path} alt="" />
+              <figcaption>{card.title}</figcaption>
+            </figure>
+          );
+        })}
+      </main>
     </div>
-
   );
 }
 
