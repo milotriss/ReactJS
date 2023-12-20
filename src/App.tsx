82ff91555c37fd38bb06abb988a5f11aec79c2db
milotriss <nhattienla98@gmail.com>
2023-12-20 08:59:53 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,21 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+
 import "./App.css";
 
-function App() {
-  const [cards, setCards] = useState<any>([]);
-  const [input,setInput] = useState<string>('');
+import Register from "./register/register";
+import Login from "./login/login";
 
-  useEffect(() => {
-    if (input.length === 0) {
-      axios
-        .get(
-          "https://api.themoviedb.org/3/search/movie?api_key=97f1bd616b51e07825e04a855aaeed30&query=${search}"
-        )
-        .then((data) => setCards(data.data.results))
-        .catch((error) => {
-          console.log(error);
-        });
-    }else{
-      onSearch()
-    }
-  }, [input]);
-  const onSearch = () => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=97f1bd616b51e07825e04a855aaeed30&query=${input}`
-      )
-      .then((data) => setCards(data.data.results))
-      .catch((error) => {
-        console.log(error);
-      });
+function App() {
+  const [onRegister, setOnRegister] = useState<boolean>(false)
+  const openRegister = ():void => {
+    setOnRegister(true);
+  }
+  const offRegister = ():void => {
+    setOnRegister(false);
   }
   return (
     <div className="App">
-      <header>
-        <input
-        onChange={(e) => setInput(e.target.value)} 
-        autoFocus 
-        placeholder="Search by name ..." 
-        type="text" />
-        <button onClick={onSearch}>Search</button>
-      </header>
-      <main>
-        {cards.map((card: any) => {
-          return (
-            <figure key={card.id}>
-              <img src={card.poster_path} alt="" />
-              <figcaption>{card.title}</figcaption>
-            </figure>
-          );
-        })}
-      </main>
+      {onRegister ? <Register  offRegister={offRegister}/> :  <Login openRegister={openRegister}/>}
     </div>
   );
 }
